b287143dc59257856b548025a3535de48fa6b15a
Shu Fai Cheung <shufai.cheung@gmail.com>
2021-09-05 17:11:16 +0800

    Update data_many_controls.R
    Fix the format

--- R/data_many_controls.R
+++ R/data_many_controls.R
@@ -1,16 +1,19 @@
 #'@title Sample dataset pa_example_3covs
-#' 
+#'
 #'@description A sample dataset for fitting a path analysis model, with three
 #'              coontrol variables.
-#' 
+#'
 #'@details Four variables (x1 to x4), and three control variables (
 #'         cov1, cov2, cov3), 100 cases.
-#' 
+#'
 #'Sample model to fit (in lavaan notation)
 #'
-#'mod <- 
-#'  'x3 ~  x1 + x2 + cov1 +cov2 + cov3
-#'   x4 ~  x1 + x3 + cov1 +cov2 + cov3
-#'  '
-#'   
+#'```
+#'mod <-
+#''
+#'x3 ~  x1 + x2 + cov1 +cov2 + cov3
+#'x4 ~  x1 + x3 + cov1 +cov2 + cov3
+#''
+#'```
+#'
 "pa_example_3covs"
