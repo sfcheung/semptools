d1fe36d827ab1c365d6c5b1e3ce934b5a6e17a84
Shu Fai Cheung <shufai.cheung@gmail.com>
2023-10-14 12:03:33 +0800

    WIP

---
+++ tests/testthat/test-layout_helpers.R
@@ -0,0 +1,153 @@
+skip("WIP")
+
+#' @title One Line Title
+#'
+#' @description One paragraph description
+#'
+#' @details Details
+#'   (Include subjects for verbs.)
+#'   (Use 3rd person forms for verbs.)
+#'
+#' @return
+#' Specify what are returned.
+#'
+#' @param semPaths_plot Argument description.
+#' @param object Additional arguments.
+#'
+#'
+#' @seealso [functionname()]
+#'
+#' @examples
+#' \donttest{
+#' }
+#'
+#' @export
+
+add_object <- function(semPaths_plot,
+                       object) {
+    if (!inherits(semPaths_plot, "qgraph")) {
+        stop("'semPaths_plot' not a qgraph object.")
+      }
+    attr(semPaths_plot, "semptools_fit_object") <- object
+    semPaths_plot
+  }
+
+#' @title One Line Title
+#'
+#' @description One paragraph description
+#'
+#' @details Details
+#'   (Include subjects for verbs.)
+#'   (Use 3rd person forms for verbs.)
+#'
+#' @return
+#' Specify what are returned.
+#'
+#' @param semPaths_plot Argument description.
+#' @param object Additional arguments.
+#'
+#'
+#' @seealso [functionname()]
+#'
+#' @examples
+#' \donttest{
+#' }
+#'
+#' @export
+
+auto_indicator_layout <- function(semPaths_plot,
+                                  object) {
+    if (!inherits(semPaths_plot, "qgraph")) {
+        stop("'semPaths_plot' not a qgraph object.")
+      }
+    tmp <- attr(semPaths_plot, "semptools_fit_object")
+    if (!is.null(tmp)) {
+        object <- attr(semPaths_plot, "semptools_fit_object")
+      } else {
+        if (missing(object)) {
+            stop("'object' must be set if not stored in 'semPaths_plot'.")
+          }
+        semPaths_plot <- add_object(semPaths_plot,
+                                    object = object)
+      }
+    semPaths_plot
+  }
+
+library(lavaan)
+library(semPlot)
+
+# CFA
+
+mod <-
+  'f1 =~ x01 + x02 + x03
+   f2 =~ x04 + x05 + x06 + x07
+   f3 =~ x08 + x09 + x10
+   f4 =~ x11 + x12 + x13 + x14
+  '
+fit <- lavaan::cfa(mod, cfa_example)
+p <- semPaths(fit, whatLabels="est",
+        sizeMan = 3.25,
+        node.width = 1,
+        edge.label.cex = .75,
+        mar = c(10, 5, 10, 5),
+        DoNotPlot = TRUE)
+p <- add_object(p, fit)
+plot(p)
+indicator_order  <- c("x04", "x05", "x06", "x07",
+                      "x01", "x02", "x03",
+                      "x11", "x12", "x13", "x14",
+                      "x08", "x09", "x10")
+indicator_factor <- c( "f2",  "f2",  "f2",  "f2",
+                       "f1",  "f1",  "f1",
+                       "f4",  "f4",  "f4",  "f4",
+                       "f3",  "f3",  "f3")
+parameterTable(fit)
+test_that("LISREL", {
+    expect_no_warning(p2 <- set_cfa_layout(p,
+                                           indicator_order,
+                                           indicator_factor))
+  })
+
+
+# SEM
+
+mod <-
+  'f1 =~ x01 + x02 + x03
+   f2 =~ x04 + x05 + x06 + x07
+   f3 =~ x08 + x09 + x10
+   f4 =~ x11 + x12 + x13 + x14
+   f3 ~  f1 + f2
+   f4 ~  f1 + f3
+  '
+fit <- lavaan::sem(mod, cfa_example)
+p <- semPaths(fit, whatLabels="est",
+        sizeMan = 5,
+        node.width = 1,
+        edge.label.cex = .75,
+        style = "lisrel",
+        mar = c(5, 5, 5, 5),
+        DoNotPlot = TRUE)
+indicator_order  <- c("x04", "x05", "x06", "x07",
+                      "x01", "x02", "x03",
+                      "x11", "x12", "x13", "x14",
+                      "x08", "x09", "x10")
+indicator_factor <- c( "f2",  "f2",  "f2",  "f2",
+                       "f1",  "f1",  "f1",
+                       "f4",  "f4",  "f4",  "f4",
+                       "f3",  "f3",  "f3")
+factor_layout <- layout_matrix(f1 = c(1, 1),
+                               f2 = c(3, 1),
+                               f3 = c(2, 2),
+                               f4 = c(2, 3))
+factor_point_to <- layout_matrix(left = c(1, 1),
+                                 left = c(3, 1),
+                                 down = c(2, 2),
+                                 down = c(2, 3))
+
+test_that("LISREL", {
+    expect_no_warning(p2 <- set_sem_layout(p,
+                                           indicator_order = indicator_order,
+                                           indicator_factor = indicator_factor,
+                                           factor_layout = factor_layout,
+                                           factor_point_to = factor_point_to))
+  })
