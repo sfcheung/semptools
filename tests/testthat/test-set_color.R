07675b029f8c1767ae18b8f53002f469c1b8b61a
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-07 12:33:00 +0800

    Add tests for set_edge_color on edges not in the model
    tests passed.

--- tests/testthat/test-set_color.R
+++ tests/testthat/test-set_color.R
@@ -31,6 +31,49 @@ p1 <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
                         "x4 ~ x2" = "yellow"))
 # plot(p1)
 
+# An edge (node) not in the plot
+p1b <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
+                        "x4~~ x3" = "black",
+                        "x3 ~ x1" = "white",
+                        "x4 ~ x1" = "darkgreen",
+                        "x4 ~ x2" = "yellow",
+                        "x6 ~ x4" = "blue"))
+# plot(p1b)
+
+# A unidrectional edge specified as a bidirectional edge
+p1c <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
+                        "x4~~ x3" = "black",
+                        "x3 ~~ x1" = "white",
+                        "x4 ~ x1" = "darkgreen",
+                        "x4 ~ x2" = "yellow"))
+# plot(p1c)
+
+# A unidrectional edge specified as a bidirectional edge,
+# wrong direction
+p1d <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
+                        "x4~~ x3" = "black",
+                        "x1 ~~ x3" = "white",
+                        "x4 ~ x1" = "darkgreen",
+                        "x4 ~ x2" = "yellow",
+                        "x6 ~ x4" = "blue"))
+# plot(p1d)
+
+# A bidrectional edge specified as a unidirectional edge,
+p1e <- set_edge_color(p_pa, c("x1 ~ x2" = "red",
+                        "x4~~ x3" = "black",
+                        "x3 ~ x1" = "white",
+                        "x4 ~ x1" = "darkgreen",
+                        "x4 ~ x2" = "yellow"))
+# plot(p1e)
+
+# A bidrectional edge specified as a unidirectional edge,
+p1f <- set_edge_color(p_pa, c("x2 ~ x1" = "red",
+                        "x4~~ x3" = "black",
+                        "x3 ~ x1" = "white",
+                        "x4 ~ x1" = "darkgreen",
+                        "x4 ~ x2" = "yellow"))
+# plot(p1f)
+
 test_that("set_edge_color", {
     expect_equal(p2$graphAttributes$Edges$color,
                  c("blue", "#808080FF", "#808080FF", "#808080FF", "#808080FF",
@@ -39,6 +82,17 @@ test_that("set_edge_color", {
     expect_equal(p1$graphAttributes$Edges$color,
                  c("red", "white", "#808080FF", "darkgreen", "yellow", "#808080FF",
                    "#808080FF", "#808080FF", "#808080FF", "black", "red", "black"))
+    expect_equal(p1$graphAttributes$Edges$color,
+                 p1b$graphAttributes$Edges$color)
+    expect_equal(p1$graphAttributes$Edges$color,
+                 p1c$graphAttributes$Edges$color)
+    expect_false(identical(
+                 p1$graphAttributes$Edges$color,
+                 p1d$graphAttributes$Edges$color))
+    expect_equal(p1$graphAttributes$Edges$color,
+                 p1e$graphAttributes$Edges$color)
+    expect_equal(p1$graphAttributes$Edges$color,
+                 p1f$graphAttributes$Edges$color)
   })
 
 tmp <- list(list(from = "x1",
