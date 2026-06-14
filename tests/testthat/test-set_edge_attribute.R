40a8bd9a2af6e5d698275b52f391c64957c9b745
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-15 06:45:43 +0800

    Update tests for using either label or name
    tests passed.

--- tests/testthat/test-set_edge_attribute.R
+++ tests/testthat/test-set_edge_attribute.R
@@ -1,19 +1,22 @@
 library(lavaan)
 library(semPlot)
 
+dat <- pa_example
+colnames(dat) <- gsub("x3", "TheX3", colnames(dat))
+
 mod_pa <-
  'x1 ~~ x2
-  x3 ~  x1 + x2
+  TheX3 ~  x1 + x2
   x4 ~  x1 + x2
  '
-fit_pa <- lavaan::sem(mod_pa, pa_example)
+fit_pa <- lavaan::sem(mod_pa, dat)
 
-m <- matrix(c("x1",  NA, "x3",
+m <- matrix(c("x1",  NA, "TX3",
               "x2",  NA, "x4"), byrow = TRUE, 2, 3)
 p_pa <- semPaths(fit_pa, whatLabels = "est",
            sizeMan = 10,
            edge.label.cex = 1.15,
-           nCharNodes = 0,
+#           nCharNodes = 0,
            nCharEdges = 0,
            layout = m,
            DoNotPlot = TRUE)
@@ -21,13 +24,22 @@ p_pa <- semPaths(fit_pa, whatLabels = "est",
 subset(parameterEstimates(fit_pa), op == "~~")
 
 p2 <- set_edge_attribute(p_pa, c("x2~~x1" = "blue",
-                                 "x3~~ x4" = rgb(0, 1, 0)),
+                                 "TX3~~ x4" = rgb(0, 1, 0)),
                          attribute_name = "color")
-# plot(p2)
+p2b <- set_edge_attribute(p_pa, c("x2~~x1" = "blue",
+                                 "TheX3~~ x4" = rgb(0, 1, 0)),
+                         attribute_name = "color")
+
+expect_equal(
+  p2$graphAttributes$Edges$color,
+  p2b$graphAttributes$Edges$color
+)
+
+# plot(p2b)
 
 p1 <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
-                                 "x4~~ x3" = "black",
-                                 "x3 ~ x1" = "white",
+                                 "x4~~ TX3" = "black",
+                                 "TX3 ~ x1" = "white",
                                  "x4 ~ x1" = "darkgreen",
                                  "x4 ~ x2" = "yellow"),
                          attribute_name = "color")
@@ -37,16 +49,16 @@ p1 <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
 # An edge not in the model will be skipped
 
 p2b <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
-                                 "x4~~ x3" = "black",
-                                 "x3 ~ x1" = "white",
+                                 "x4~~ TX3" = "black",
+                                 "TX3 ~ x1" = "white",
                                  "x1 ~ x4" = "blue",
                                  "x4 ~ x2" = "yellow"),
                          attribute_name = "color")
 # plot(p2b)
 
 p2c <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
-                                 "x4~~ x3" = "black",
-                                 "x3 ~ x1" = "white",
+                                 "x4~~ TX3" = "black",
+                                 "TX3 ~ x1" = "white",
                                  "x1 ~~ x4" = "blue",
                                  "x4 ~ x2" = "yellow"),
                          attribute_name = "color")
@@ -54,8 +66,8 @@ p2c <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
 
 # Direction ignored
 p2d <- set_edge_attribute(p_pa, c("x1 ~~x2" = "red",
-                                 "x4~~ x3" = "black",
-                                 "x3 ~~ x1" = "white",
+                                 "x4~~ TX3" = "black",
+                                 "TX3 ~~ x1" = "white",
                                  "x4 ~~ x1" = "darkgreen",
                                  "x4 ~~ x2" = "yellow"),
                          attribute_name = "color",
@@ -81,7 +93,7 @@ test_that("set_edge_attribute: color", {
   })
 
 p3 <- set_edge_attribute(p_pa, c("x1 ~~x2" = -3,
-                                 "x3 ~ x1" = 4),
+                                 "TX3 ~ x1" = 4),
                          attribute_name = "curve")
 
 # plot(p3)
@@ -92,7 +104,7 @@ test_that("set_edge_attribute: curve", {
   })
 
 p4 <- set_edge_attribute(p_pa, c("x1 ~~x2" = 2,
-                                 "x3 ~ x1" = 5),
+                                 "TX3 ~ x1" = 5),
                          attribute_name = "label.cex")
 
 # plot(p4)
