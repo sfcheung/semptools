5f1506e2f6954942200856dc80a0b6af0967f876
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-15 20:59:23 +0800

    Update tests for abbreviated names
    tests passed.

--- tests/testthat/test-set_color.R
+++ tests/testthat/test-set_color.R
@@ -1,19 +1,23 @@
 library(lavaan)
 library(semPlot)
 
+dat <- pa_example
+colnames(dat) <- gsub("x3", "TheX3", colnames(dat))
+
+library(lavaan)
 mod_pa <-
  'x1 ~~ x2
-  x3 ~  x1 + x2
+  TheX3 ~  x1 + x2
   x4 ~  x1 + x2
  '
-fit_pa <- lavaan::sem(mod_pa, pa_example)
+fit_pa <- lavaan::sem(mod_pa, dat)
 
-m <- matrix(c("x1",  NA, "x3",
+m <- matrix(c("x1",  NA, "TX3",
               "x2",  NA, "x4"), byrow = TRUE, 2, 3)
 p_pa <- semPaths(fit_pa, whatLabels = "est",
            sizeMan = 10,
            edge.label.cex = 1.15,
-           nCharNodes = 0,
+          #  nCharNodes = 0,
            nCharEdges = 0,
            layout = m,
            DoNotPlot = TRUE)
@@ -21,20 +25,20 @@ p_pa <- semPaths(fit_pa, whatLabels = "est",
 subset(parameterEstimates(fit_pa), op == "~~")
 
 p2 <- set_edge_color(p_pa, c("x2~~x1" = "blue",
-                        "x3~~ x4" = rgb(0, 1, 0)))
+                        "TX3~~ x4" = rgb(0, 1, 0)))
 # plot(p2)
 
 p1 <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
-                        "x4~~ x3" = "black",
-                        "x3 ~ x1" = "white",
+                        "x4~~ TX3" = "black",
+                        "TX3 ~ x1" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow"))
 # plot(p1)
 
 # An edge (node) not in the plot
 p1b <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
-                        "x4~~ x3" = "black",
-                        "x3 ~ x1" = "white",
+                        "x4~~ TX3" = "black",
+                        "TX3 ~ x1" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow",
                         "x6 ~ x4" = "blue"))
@@ -42,8 +46,8 @@ p1b <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
 
 # A unidrectional edge specified as a bidirectional edge
 p1c <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
-                        "x4~~ x3" = "black",
-                        "x3 ~~ x1" = "white",
+                        "x4~~ TX3" = "black",
+                        "TX3 ~~ x1" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow"))
 # plot(p1c)
@@ -51,8 +55,8 @@ p1c <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
 # A unidrectional edge specified as a bidirectional edge,
 # wrong direction
 p1d <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
-                        "x4~~ x3" = "black",
-                        "x1 ~~ x3" = "white",
+                        "x4~~ TX3" = "black",
+                        "x1 ~~ TX3" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow",
                         "x6 ~ x4" = "blue"))
@@ -60,16 +64,16 @@ p1d <- set_edge_color(p_pa, c("x1 ~~x2" = "red",
 
 # A bidrectional edge specified as a unidirectional edge,
 p1e <- set_edge_color(p_pa, c("x1 ~ x2" = "red",
-                        "x4~~ x3" = "black",
-                        "x3 ~ x1" = "white",
+                        "x4~~ TX3" = "black",
+                        "TX3 ~ x1" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow"))
 # plot(p1e)
 
 # A bidrectional edge specified as a unidirectional edge,
 p1f <- set_edge_color(p_pa, c("x2 ~ x1" = "red",
-                        "x4~~ x3" = "black",
-                        "x3 ~ x1" = "white",
+                        "x4~~ TX3" = "black",
+                        "TX3 ~ x1" = "white",
                         "x4 ~ x1" = "darkgreen",
                         "x4 ~ x2" = "yellow"))
 # plot(p1f)
