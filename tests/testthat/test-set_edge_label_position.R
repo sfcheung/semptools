2560112d3d4536037f799713d059a9f36ad80f48
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-15 20:56:10 +0800

    Update tests for abbreviated names
    tests passed.

--- tests/testthat/test-set_edge_label_position.R
+++ tests/testthat/test-set_edge_label_position.R
@@ -1,19 +1,22 @@
 library(lavaan)
 library(semPlot)
 
+dat <- pa_example
+colnames(dat) <- gsub("x3", "TheX3", colnames(dat))
+
 mod_pa <-
  'x1 ~~ x2
-  x3 ~  x1 + x2
+  TheX3 ~  x1 + x2
   x4 ~  x1 + x2
  '
-fit_pa <- lavaan::sem(mod_pa, pa_example)
+fit_pa <- lavaan::sem(mod_pa, dat)
 
-m <- matrix(c("x1",  NA, "x3",
+m <- matrix(c("x1",  NA, "TX3",
               "x2",  NA, "x4"), byrow = TRUE, 2, 3)
 p_pa <- semPaths(fit_pa, whatLabels = "est",
            sizeMan = 10,
            edge.label.cex = 1.15,
-           nCharNodes = 0,
+          #  nCharNodes = 0,
            nCharEdges = 0,
            layout = m,
            DoNotPlot = TRUE)
@@ -23,12 +26,12 @@ subset(parameterEstimates(fit_pa), op == "~~")
 p2 <- set_edge_label_position(
         p_pa,
         c("x1~~x2" = .70,
-          "x3~~ x3" = .10))
+          "TX3~~ TX3" = .10))
 # plot(p2)
 p2b <- set_edge_label_position(
         p_pa,
         c("x2~~x1" = .70,
-          "x3~~ x3" = .10))
+          "TX3~~ TX3" = .10))
 # plot(p2b)
 
 test_that("set_edge_lable_position", {
