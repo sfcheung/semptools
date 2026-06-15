62456b803de86327f1fba0811b97a4def9c934d5
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-15 22:33:58 +0800

    Update tests for abbreviated names
    tests passed.

--- tests/testthat/test-set_node_attribute.R
+++ tests/testthat/test-set_node_attribute.R
@@ -1,36 +1,47 @@
 # Test set_node_attribute
 
+# set_node_attribute works with both names and abbreviated names
+# set_node_color works like set_node_attribute
+# set_node_label_size works like set_node_attribute
+# set_node_label_color works like set_node_attribute
+# set_node_shape works like set_node_attribute
+# set_node_size and friends works like set_node_attribute
+
+# rotate_resid works only with original names
+
 dat <- pa_example
 colnames(dat) <- gsub("x3", "TheX3", colnames(dat))
+colnames(dat) <- gsub("x4", "TheX4", colnames(dat))
 
 library(lavaan)
 mod_pa <-
  'x1 ~~ x2
   TheX3 ~  x1 + x2
-  x4 ~  x1 +
+  TheX4 ~  x1 + x2
  '
 fit_pa <- lavaan::sem(mod_pa, dat)
 
 library(semPlot)
 m <- matrix(c("x1",   NA,  NA,   NA,
-                NA, "TX3",  NA, "x4",
+                NA, "TX3",  NA, "TX4",
               "x2",   NA,  NA,   NA), byrow = TRUE, 3, 4)
 p_pa <- semPaths(fit_pa, whatLabels = "est",
            sizeMan = 10,
            edge.label.cex = 1.15,
            # nCharNodes = 0,
            nCharEdges = 0,
-           layout = m, DoNotPlot = TRUE)
+           layout = m,
+           DoNotPlot = TRUE)
 # plot(p_pa)
 
-my_rotate_resid_list_a <- list(list(node = "TheX3", new_value =  45 * pi / 180),
-                               list(node = "x4", new_value = -45 * pi / 180),
+my_rotate_resid_list_a <- list(list(node = "TX3", new_value =  45 * pi / 180),
+                               list(node = "TheX4", new_value = -45 * pi / 180),
                                list(node = "x2", new_value = -90 * pi / 180))
 my_rotate_resid_list_b <- list(list(node = "TheX3", rotate =  45),
-                               list(node = "x4", rotate = -45),
+                               list(node = "TheX4", rotate = -45),
                                list(node = "x2", rotate = -90))
-my_rotate_resid_list_c <- c("TX3" =  45 * pi / 180,
-                            "x4" = -45 * pi / 180,
+my_rotate_resid_list_c <- c("TheX3" =  45 * pi / 180,
+                            "TX4" = -45 * pi / 180,
                             "x2" = -90 * pi / 180)
 
 p_pa2a <- set_node_attribute(p_pa,
@@ -52,8 +63,8 @@ expect_identical(p_pa2b$graphAttributes$Nodes$loopRotation,
                  p_pa2c$graphAttributes$Nodes$loopRotation)
 })
 
-my_rotate_resid_vector_a <- c(TheX3 = 45, x4 = -45, x2 = -90) * pi / 180
-my_rotate_resid_vector_b <- c(TheX3 = 45, x4 = -45, x2 = -90)
+my_rotate_resid_vector_a <- c(TheX3 = 45, TX4 = -45, x2 = -90) * pi / 180
+my_rotate_resid_vector_b <- c(TheX3 = 45, TheX4 = -45, x2 = -90)
 
 p_pa3a <- set_node_attribute(p_pa,
                              my_rotate_resid_vector_a,
@@ -71,7 +82,7 @@ expect_identical(p_pa3b$graphAttributes$Nodes$loopRotation,
 
 test_that("set_node_attribute: label.cex", {
 
-my_node_label_cex <- c(TheX3 = 2, x4 = 3)
+my_node_label_cex <- c(TheX3 = 2, TheX4 = 3)
 p_pa4a <- set_node_attribute(p_pa,
                              my_node_label_cex,
                              attribute_name = "label.cex")
@@ -80,10 +91,10 @@ expect_identical(p_pa4a$graphAttributes$Nodes$label.cex,
                  c(2, 3, 1, 1))
 
 p_pa4b <- set_node_label_size(p_pa,
-                             c(x2 = 10, x1 = 3))
+                             c(TX3 = 10, x1 = 3))
 # plot(p_pa4b)
 expect_identical(p_pa4b$graphAttributes$Nodes$label.cex,
-                 c(1, 1, 3, 10))
+                 c(10, 1, 3, 1))
 
 p_pa4c <- set_node_label_color(p_pa,
                              c(x2 = "blue", x1 = "green"))
@@ -95,7 +106,7 @@ expect_identical(p_pa4c$graphAttributes$Nodes$label.color,
 
 test_that("set_node_attribute: color", {
 
-my_node_color <- c(TheX3 = "red", x4 = "blue")
+my_node_color <- c(TX3 = "red", TX4 = "blue")
 p_pa5a <- set_node_attribute(p_pa,
                              my_node_color,
                              attribute_name = "color")
@@ -105,7 +116,7 @@ expect_identical(p_pa5a$graphAttributes$Nodes$color,
 
 p_pa5b <- set_node_color(
   p_pa,
-  c(TheX3 = "blue", x1 = "red")
+  c(TX3 = "blue", x1 = "red")
 )
 # plot(p_pa5b)
 expect_identical(p_pa5b$graphAttributes$Nodes$color,
@@ -139,13 +150,13 @@ test_that("set_node_attribute: size", {
 
 p_pa7a <- set_node_size(
   p_pa,
-  c(x1 = 5, TheX3 = 20)
+  c(TheX4 = 5, TX3 = 20)
 )
 # plot(p_pa7a)
 expect_identical(p_pa7a$graphAttributes$Nodes$height,
-                c(20, 10, 5, 10))
+                c(20, 5, 10, 10))
 expect_identical(p_pa7a$graphAttributes$Nodes$width,
-                c(20, 10, 5, 10))
+                c(20, 5, 10, 10))
 
 p_pa7b <- set_node_width(
   p_pa,
