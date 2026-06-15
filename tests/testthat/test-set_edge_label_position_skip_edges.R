e9cf1a5a8006856d4748f3c05f43ccdfd2d1c36c
Shu Fai Cheung <shufai.cheung@gmail.com>
2026-06-15 20:54:25 +0800

    Update tests for abbreviated names
    tests passed.

--- tests/testthat/test-set_edge_label_position_skip_edges.R
+++ tests/testthat/test-set_edge_label_position_skip_edges.R
@@ -3,43 +3,46 @@ library(semPlot)
 
 test_that("set_edge_label_position: skip edges", {
 
-mod_pa <-
- 'x1 ~~ x2
-  x3 ~  x1 + x2
-  x4 ~  x1 + x2
- '
-fit_pa <- lavaan::sem(mod_pa, pa_example)
+dat <- pa_example
+colnames(dat) <- gsub("x1", "ThX1", colnames(dat))
 
-m <- matrix(c("x1",  NA, "x3",
+mod_pa <-
+ 'ThX1 ~~ x2
+  x3 ~  ThX1 + x2
+  x4 ~  ThX1 + x2
+ '
+fit_pa <- lavaan::sem(mod_pa, dat)
+
+m <- matrix(c("TX1",  NA, "x3",
               "x2",  NA, "x4"), byrow = TRUE, 2, 3)
 p_pa <- semPaths(fit_pa, whatLabels = "est",
            sizeMan = 10,
            edge.label.cex = 1.15,
-           nCharNodes = 0,
+          #  nCharNodes = 0,
            nCharEdges = 0,
            layout = m,
            DoNotPlot = TRUE)
 
 p1 <- set_edge_attribute(
         p_pa,
-        c("x4 ~ x1" = .7,
-          "x1 ~~ x2" = .8),
+        c("x4 ~ TX1" = .7,
+          "TX1 ~~ x2" = .8),
         attribute_name = "edge.label.position"
       )
 # plot(p1)
 
 p1b <- set_edge_label_position(
         p_pa,
-        c("x4 ~ x1" = .7,
-          "x1 ~~ x2" = .8)
+        c("x4 ~ TX1" = .7,
+          "TX1 ~~ x2" = .8)
       )
 # plot(p1b)
 
 # An edge not in the model
 p1c <- set_edge_label_position(
         p_pa,
-        c("x4 ~ x1" = .7,
-          "x1 ~~ x2" = .8,
+        c("x4 ~ TX1" = .7,
+          "TX1 ~~ x2" = .8,
           "x6 ~ x5" = .5)
       )
 # plot(p1c)
@@ -48,14 +51,14 @@ p1c <- set_edge_label_position(
 # The order of lhs and rhs does not matter
 p1d <- set_edge_label_position(
         p_pa,
-        c("x4 ~ x1" = .7,
-          "x1 ~ x2" = .8)
+        c("x4 ~ TX1" = .7,
+          "TX1 ~ x2" = .8)
       )
 # plot(p1d)
 p1d2 <- set_edge_label_position(
         p_pa,
-        c("x4 ~ x1" = .7,
-          "x2 ~ x1" = .8)
+        c("x4 ~ TX1" = .7,
+          "x2 ~ TX1" = .8)
       )
 # plot(p1d2)
 
@@ -63,16 +66,16 @@ p1d2 <- set_edge_label_position(
 # The order of lhs and rhs does not matter
 p1e <- set_edge_label_position(
         p_pa,
-        c("x4 ~~ x1" = .7,
-          "x1 ~ x2" = .8)
+        c("x4 ~~ TX1" = .7,
+          "TX1 ~ x2" = .8)
       )
 # plot(p1e)
 
 # A unidirectional edge specified as a directional edge
 p1f <- set_edge_label_position(
         p_pa,
-        c("x4 ~~ x1" = .7,
-          "x2 ~ x1" = .8)
+        c("x4 ~~ TX1" = .7,
+          "x2 ~ TX1" = .8)
       )
 # plot(p1f)
 
